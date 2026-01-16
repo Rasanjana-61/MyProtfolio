ef1e42692cc333c3292dabd7e293abb386326728
Rasanjana-61 <pramothrasanjana@gmail.com>
2026-01-16 15:41:09 +0530

    feat: update project descriptions and links in Portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,14 +3,14 @@ function Portfolio() {
         {
             image: '/portfolio1.jpg',
             title: 'Online food recipe management system',
-            description: 'A full-stack bag-selling e-commerce system featuring authentication, customization, real-time stock updates, secure checkout, and admin dashboards.',
+            description: 'A full-stack web application for managing online food recipes and customer orders, featuring meal browsing, recipe viewing, secure user authentication, and an admin dashboard with a responsive user-friendly interface.',
             technologies: ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL'],
-            link: 'https://github.com/Rasanjana-61/FinanceDashboard.git'
+            link: 'https://github.com/Rasanjana-61/Online-Food-Recipe-System.git'
         },
         {
             image: '/portfolio2.jpg',
             title: 'Car rental management system',
-            description: 'A wellness mobile app for habit tracking, mood journaling, hydration reminders, and progress insights.',
+            description: 'A web application for managing car rentals, allowing users to browse vehicles, select rental dates, make reservations, and manage bookings with secure authentication and an admin dashboard.',
             //technologies: ['Kotlin', 'SharedPreferences', 'Android Studio'],
             technologies: ['Java', 'JSP', 'SQL', 'Tomcat'],
             link: 'https://github.com/Rasanjana-61/CarRentalSystem.git'
@@ -18,13 +18,13 @@ function Portfolio() {
         {
             image: '/portfolio3.jpg',
             title: 'Online garment management system',
-            description: 'A web-based hotel booking system with secure backend staff and role management.',
-            technologies: ['Java', 'JSP', 'SQL', 'Tomcat'],
-            link: 'https://github.com/Rasanjana-61/CarRentalSystem.git'
+            description: 'A full-stack web application designed to manage garment production, inventory, and orders efficiently. The system allows users to manage products, track orders, handle customer requests, and monitor stock levels through a secure admin dashboard and a responsive user-friendly interface.',
+            technologies: ['React', 'Node.js', 'Express', 'MongoDB',],
+            link: 'https://github.com/Rasanjana-61/FabricLK-Garment-Management-System.git'
         },
         
         {
-            image: '/portfolio3.jpg',
+            image: '/portfolio4.jpg',
             title: 'CrownCrest – Hotel Reservation System',
             description: 'A web-based hotel booking system with secure backend staff and role management.',
             technologies: ['Java', 'JSP', 'SQL', 'Tomcat'],
