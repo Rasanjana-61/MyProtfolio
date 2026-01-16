42db6e94a00c6768bc814c6d60b32dbcbb2bf733
Rasanjana-61 <pramothrasanjana@gmail.com>
2026-01-16 15:39:44 +0530

    feat: update About section with personal developer description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
                 </div>
                 <h2 className="text-typing-about">Frontend developer</h2>
                 <div className="text-about-content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nemo quasi at corporis perspiciatis voluptatem molestiae itaque et maiores consectetur commodi atque maxime vero similique totam recusandae in, voluptates fugiat? Architecto dolore quasi natus veniam consectetur? Est autem vero hic sit, aliquam non impedit delectus.</p>
+                    <p>I am a passionate full-stack developer and IT undergraduate who enjoys building modern, scalable web applications. I have experience working with the MERN stack, Java, PHP, and cloud-based technologies. I love solving real-world problems through clean code, learning new technologies, and continuously improving my skills as a developer.</p>
                     <button>Read more</button>
                 </div>
             </div>
